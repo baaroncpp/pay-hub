cfdb723e47c1a1e89b493fef190b7fc745d43a4f
Napho <starnapho@gmail.com>
2021-02-23 11:43:10 +0300

    Add fetch district code

--- core/src/main/java/com/jajjamind/payvault/core/api/agent/CompanyApi.java
+++ core/src/main/java/com/jajjamind/payvault/core/api/agent/CompanyApi.java
@@ -2,9 +2,11 @@ package com.jajjamind.payvault.core.api.agent;
 
 import com.jajjamind.payvault.core.BaseApi;
 import com.jajjamind.payvault.core.api.agent.models.Company;
+import com.jajjamind.payvault.core.jpa.models.RecordList;
 import com.jajjamind.payvault.core.service.agent.CompanyService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
@@ -31,7 +33,7 @@ public class CompanyApi implements BaseApi<Company> {
         return companyService.add(company);
     }
 
-    @RolesAllowed("ROLE_COMPANY.READ")
+    @RolesAllowed({"ROLE_COMPANY.READ","ROLE_COMPANY.WRITE"})
     @Override
     public Company get( Long id) {
         return companyService.get(id);
@@ -47,10 +49,29 @@ public class CompanyApi implements BaseApi<Company> {
     public Company delete( long id) {
         return companyService.delete(id);
     }
-    @RolesAllowed("ROLE_COMPANY.READ")
+
+    @RolesAllowed({"ROLE_COMPANY.READ","ROLE_COMPANY_WRITE"})
     @Override
     public List<Company> getAll() {
 
         return companyService.getAll();
+    }
+
+    @RolesAllowed({"ROLE_COMPANY.READ","ROLE_COMPANY.WRITE"})
+    @GetMapping(value = "/by-tin/{tinNumber}",produces = APPLICATION_JSON)
+    public Company getCompanyByTinNumber(@PathVariable("tinNumber") String tinNumber){
+        return companyService.findByTinNumber(tinNumber);
+    }
+
+    @RolesAllowed({"ROLE_COMPANY.READ","ROLE_COMPANY.WRITE"})
+    @GetMapping(value = "/by-businessname/{businessName}",produces = APPLICATION_JSON)
+    public Company getCompanyByBusinessName(@PathVariable("businessName") String businessName){
+        return companyService.findByBusinessName(businessName);
+    }
+
+    @RolesAllowed({"ROLE_COMPANY.READ","ROLE_COMPANY.WRITE"})
+    @GetMapping("/query")
+    public RecordList getAllCompanies(@RequestParam MultiValueMap map){
+        return companyService.getAllCompanies(map);
     }
 }
--- core/src/main/java/com/jajjamind/payvault/core/api/location/DistrictApi.java
+++ core/src/main/java/com/jajjamind/payvault/core/api/location/DistrictApi.java
@@ -1,6 +1,15 @@
 package com.jajjamind.payvault.core.api.location;
 
+import com.jajjamind.payvault.core.BaseApi;
+import com.jajjamind.payvault.core.api.agent.models.District;
+import com.jajjamind.payvault.core.service.agent.DistrictService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author akena
@@ -8,8 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
  * 15:04
  **/
 
+@Tag(name = "Districts",description = "Get all districts that are in the system")
 @RestController
+@RequestMapping("/district")
 public class DistrictApi {
 
+    @Autowired
+    DistrictService districtService;
+
+    @GetMapping(value = "/all",produces = BaseApi.APPLICATION_JSON)
+    List<District> getAllDistricts(){
+        return districtService.getAllDistricts();
+    }
 
 }
