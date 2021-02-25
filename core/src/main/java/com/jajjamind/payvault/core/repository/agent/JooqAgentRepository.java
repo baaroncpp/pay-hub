8921dab64ba5891574da6ca92b746a39deade659
Napho <starnapho@gmail.com>
2021-02-26 02:42:17 +0300

    Correct jooq null pointer exception

--- core/src/main/java/com/jajjamind/payvault/core/repository/agent/JooqAgentRepository.java
+++ core/src/main/java/com/jajjamind/payvault/core/repository/agent/JooqAgentRepository.java
@@ -252,7 +252,7 @@ public class JooqAgentRepository implements QueryResultPicker{
 
     public RecordList<Result> listAndCount(MultiValueMap<String,?> map){
         final RecordList<Result> result  = new RecordList<Result>(count(map),list(map));
-        result.setOffset(map.containsKey("offset") ? Integer.valueOf((String)map.getFirst("offset")) : null);
+        result.setOffset(map.containsKey("offset") ? Integer.valueOf((String)map.getFirst("offset")) : 0);
         result.setRecordsFiltered(result.getRecords().size());
         return result;
     }
